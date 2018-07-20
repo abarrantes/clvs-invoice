b84b2ea893dba22cd2dcad69044de3a75f500356
andrey <abarrantes@clavisco.com>
2018-07-20 11:17:06 -0400

    fixed seed files to work in heroku

--- bin/seedsCustomer.js
+++ bin/seedsCustomer.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Customer = require('../models/customer');
 
 // it was needed because app.js is not related to this seeds.js file, we run it independently with node bin/seeds.js
-mongoose.connect('mongodb://localhost/clvs-invoice');
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost/clvs-invoice');
+mongoose.connect(process.env.MONGODB_URI);
 
 const customers = [
   {
